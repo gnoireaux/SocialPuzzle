67044d7cde2ce6cea84f2cb8c7aede88fd0c10ac
Guillaume Noireaux <gnoireaux@hnp.local>
2010-10-04 20:47:03 +0200

    refactored out redundant test

--- spec/word_friends_spec.rb
+++ spec/word_friends_spec.rb
@@ -6,9 +6,6 @@ describe WordFriends do
     word_friends = WordFriends.new('causes', %w{causes})
     word_friends.network_size.should == 1
   end
-
-  it "should accept a word list" do
-    WordFriends.new('causes', %w{causes} )
-  end
+  
 end
 
