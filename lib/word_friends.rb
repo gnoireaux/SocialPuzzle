918361c53dc9341b1f1950ead225cd69538c907f
Guillaume Noireaux <gnoireaux@hnp.local>
2010-10-04 20:52:29 +0200

    recognizes as friend a deletion at the end of the word

--- lib/word_friends.rb
+++ lib/word_friends.rb
@@ -1,9 +1,14 @@
 class WordFriends
   def initialize(word, list)
-    
+    @word = word
+    @list = list
   end
 
   def network_size
-    1
+    if @list.include? @word.sub(/.$/,'')
+      2
+    else
+      1
+    end
   end
 end
--- spec/word_friends_spec.rb
+++ spec/word_friends_spec.rb
@@ -6,6 +6,11 @@ describe WordFriends do
     word_friends = WordFriends.new('causes', %w{causes})
     word_friends.network_size.should == 1
   end
+
+  it "should consider last letter off, a friend" do
+    word_friends = WordFriends.new('causes', %w{cause})
+    word_friends.network_size.should == 2
+  end
   
 end
 
